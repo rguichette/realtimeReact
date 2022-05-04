cdef4ae9e3737058f33e2037aa38a36c364e2205
ralph <ralphgui@gmail.com>
2022-05-03 20:41:04 -0400

    started ice exchange

--- server/server.ts
+++ server/server.ts
@@ -14,12 +14,31 @@ const io = new Server(httpServer, { cors: {
 let port = 5000;
 
 io.on("connection", (socket) => {
-    // ...
     console.log("connected");
-    
+
+    //listen to offer and send call details (offer) to recipient
+    socket.on('offer', (data)=>{
+      // console.log(data);
+      socket.to(data.callMeta.callee).emit("offer",{offer: data.offer, caller: data.callMeta.caller} )
+      
+    })
+
+    socket.on('answer', (data)=>{
+      socket.to(data.caller).emit("answer", {answer:data.answer})
+    // console.log(data);
+
+    })
+
+    socket.on('candidate', (data)=>{
+      console.log(data);
+      
+      socket.to(data.callMeta.callee).emit('candidate', data)
+    })
+
+
   });
 
-
+   
 
 
 // TODO: remember to send static file HERE
